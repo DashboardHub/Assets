5fcbb6dc42fe4d9753b86baeedbdb76ad3221af3
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-04-19 22:30:10 +0100

    Put logo on holding page

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<?php
-echo 'Please see https://github.com/DashboardHub/Assets';
-
+<?php ?>
+<div style="text-align: center">
+    <a href="https://github.com/DashboardHub/Assets">
+        <img src="http://cdn.dashboardhub.io/logo-vertical-std-1872x1433.png" alt="Dashboard Hub Logo" style="height:100%" />
+    </a>
+</div>
